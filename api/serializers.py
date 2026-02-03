27dba9628d43650f747db4885bd92eafb26a529f
mhamza-giga <m.hamza@gigalabs.co>
2026-02-03 12:00:57 +0500

    initial commit

--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Greeting
-
-class GreetingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Greeting
-        fields = '__all__'
